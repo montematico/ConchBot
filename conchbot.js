50cfa1a66aa147d1f0ee4f806d527f047a74499a
montematico <bradbad1970@gmail.com>
2019-06-25 22:47:36 -0400

    Fixed it for real this time

--- conchbot.js
+++ conchbot.js
@@ -1,22 +1,16 @@
 //conchbot replies with no
 
-// Import the discord.js module
 const Discord = require('discord.js');
-
-// Create an instance of a Discord client
 const client = new Discord.Client();
+const config = require('./config.json');
 
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
 // Create an event listener for messages
 client.on('message', message => {
-  // If the message is "ping"
+  // If the message ends with ? reponds with no or ask again
   if (message.content.endsWith('?')) {
     if (Math.floor((Math.random() * 10) + 1) == 5) {
       message.channel.send('Try asking again.');
@@ -26,5 +20,5 @@ client.on('message', message => {
   }
 });
 
-// Log our bot in using the token from https://discordapp.com/developers/applications/me
-client.login('NTkzMjMzNzEwODcxMDg1MDYz.XRK6sA.zzPg8bHQFBZcIIxqjYo30rZK9mI');
+
+client.login('config.token');

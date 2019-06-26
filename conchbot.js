a28785ebc91d02552c842c831d2cceb779a09fab
montematico <bradbad1970@gmail.com>
2019-06-26 19:01:41 -0400

    Update conchbot.js

--- conchbot.js
+++ conchbot.js
@@ -7,20 +7,27 @@ const config = require('./config.json');
 client.on('ready', () => {
   console.log('I am ready!');
 });
+
+var uses = 0 //Says how many times to bot has answered a question since it last stopped
+
+
 // Create an event listener for messages
 client.on('message', message =>
 {
   // If the message ends with ? reponds with no or ask again
   if (message.content.endsWith('?'))
   {
+    //prints how many times to bot has been used
+    uses += 1;
+    console.log('Answered: ' + uses);
 
-    if (Math.floor((Math.random() * 5) + 1) === 5)
+    if (Math.floor((Math.random() * 5) + 1) === 2)
     {
       message.channel.send('Try asking again.');
     } else
-      {
+    {
       message.channel.send('no.');
-      }
+    }
   }
 });
 

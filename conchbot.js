f7bad3b816954b711f785c4679281b6c687bc3b9
montematico <bradbad1970@gmail.com>
2019-06-28 10:55:51 -0400

    Oops I was working on an old version

--- conchbot.js
+++ conchbot.js
@@ -1,49 +1,25 @@
 //conchbot replies with no
-require('dotenv').load();
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-//const config = require('./config.json');
-const Dotenv = require('dotenv');
-
-
-
-require('http').createServer().listen(3000);
+require('dotenv').config()
 
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log('Logged in');
 });
-
-
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-today = mm + '/' + dd + '/' + yyyy;
-var uses = 0 //Says how many times to bot has answered a question since it last stopped
-
 
 // Create an event listener for messages
-client.on('message', message =>
-{
-    // If the message ends with ? reponds with no or ask again
-    if (message.content.endsWith('?'))
-    {
-        //prints how many times to bot has been used
-        uses += 1;
-        console.log('Answered: ' + uses);
-        console.log(time + '  ' +  today);
-
-        if (Math.floor((Math.random() * 5) + 1) === 2)
-        {
-            message.channel.send('Try asking again.');
-        } else
-        {
-            message.channel.send('no.');
-        }
+client.on('message', message => {
+  // If the message ends with ? reponds with no or ask again
+  if (message.content.endsWith('?')) {
+    if (Math.floor((Math.random() * 10) + 1) == 5) {
+      message.channel.send('Try asking again.');
+    } else {
+      message.channel.send('no.');
     }
+  }
 });
 
+
+client.login(process.env.token);
 require('http').createServer().listen(3000)
-client.login(process.env.Token);
